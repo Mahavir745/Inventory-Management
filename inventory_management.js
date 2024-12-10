6c471e8ab627d90554f11a7bc991cd893558a080
Mahavir745 <mahavir777kumar@gmail.com>
2024-12-10 12:48:26 +0530

    json format has been added

--- inventory_management.js
+++ inventory_management.js
@@ -1,7 +1,7 @@
 //todo: Product Class
 
 class Product {
-  constructor(id,name,price,expiry_date,quantity,category,supplier){
+  constructor(id, name, price, expiry_date, quantity, category, supplier) {
     this.id = id;
     this.name = name;
     this.price = price;
@@ -13,13 +13,13 @@ class Product {
 
   //todo: product as a JSON object
 
-  toJSON(){
+  toJSON() {
     return {
       id: this.id,
       name: this.name,
       price: this.price,
-      expiry_date : this.expiry_date,
-      quantity:this.quantity,
+      expiry_date: this.expiry_date,
+      quantity: this.quantity,
       category: this.category,
       supplier: {
         name: this.supplier.name,
@@ -31,19 +31,19 @@ class Product {
 
 //todo: Category Class
 
-class Category{
-  constructor(name){
+class Category {
+  constructor(name) {
     this.name = name;
     this.subcategories = [];
   }
 
-  addSubcategory(subcategory){
+  addSubcategory(subcategory) {
     this.subcategories.push(subcategory)
   }
 
   // todo: category as json
-  toJSON(){
-    return{
+  toJSON() {
+    return {
       name: this.name,
       subcategories: this.subcategories
     }
@@ -53,51 +53,67 @@ class Category{
 //todo: Supplier class
 
 class Supplier {
-  constructor(name,contactInfo){
+  constructor(name, contactInfo) {
     this.name = name;
     this.contactInfo = contactInfo;
     this.products = [];
   }
 
-  addProduct(product){
+  addProduct(product) {
     this.products.push(product)
   }
 
   // todo: supplier as json
-  toJSON(){
-    return{
+  toJSON() {
+    return {
       name: this.name,
       contactInfo: this.contactInfo,
-      products:this.products
+      products: this.products
     }
   }
 }
 
+//todo: product not found 
+
+class ProductNotFound {
+  constructor(status, message) {
+    this.status = status
+    this.message = message
+  }
+
+  toJSON() {
+    return {
+      status: this.status,
+      message: this.message
+    }
+  }
+}
 
 //todo: Inventory system Management
 
-class InventorySystemManagement{
-  constructor(){
+class InventorySystemManagement {
+  constructor() {
     this.products = [];
   }
 
-  addProduct(id,name,price,expiry_date,quantity,category,supplier){
-    const product = new Product(id,name,price,expiry_date,quantity,category,supplier)
+  addProduct(id, name, price, expiry_date, quantity, category, supplier) {
+    const product = new Product(id, name, price, expiry_date, quantity, category, supplier)
     this.products.push(product);
   }
 
   getProductDetails(name) {
-    const product = this.products.find((items)=>{
+    const product = this.products.find((items) => {
       return items.name.toLowerCase() === name.toLowerCase();
     })
 
-    if(product){
+    if (product) {
       //todo: product found
       return JSON.stringify(product.toJSON(), null, 2);
     }
-    else{
-      console.log("Product not found. Try again!")
-      return null
+    else {
+
+      let error = new ProductNotFound("error", "Product is not found")
+      return JSON.stringify(error.toJSON(),null,2)
     }
   }
 
@@ -113,9 +129,11 @@ category1.addSubcategory("Mobile");
 category1.addSubcategory("Laptop");
 
 const inventorySystem = new InventorySystemManagement();
-inventorySystem.addProduct(1,'phone',999.99, '01-01-2025',100,category1,supplier1)
+inventorySystem.addProduct(1, 'phone', 999.99, '01-01-2025', 100, category1, supplier1)
 supplier1.addProduct(category1.name)
-let data = inventorySystem.getProductDetails('phone')
+
+
+let data = inventorySystem.getProductDetails('phomne')
 
 
 //todo: Output in JSON format
